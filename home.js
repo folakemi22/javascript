ca5dc8d20ed951b708de11d521ac382646f55fb4
george <georgeolumorotifolakemi5@gmail.com>
2022-04-29 19:54:04 -0700

    my assignment

---
+++ home.js
@@ -0,0 +1,61 @@
+//1. create a function name and print sum of an arrays
+function printSum(){
+    const array = [5, 8, 9, 4];
+    let sum = 0;
+    for ( let i = 0; i < array.length; i++)
+    sum += array[i] 
+    console.log(sum)
+}
+printSum()
+
+//2.  a=(a[0], a[1], a[2]) and the rating for Bob's challenge is the triplet b=(b[0], b[1], b[2]). 
+function awardPoints(){
+    let a = [0, 1, 2];
+    let b = [0, 1, 2];
+    let totalPoint = []
+    let aPoint = 0;
+    let bpoint = 0;
+    for (let i = 0; i < Array.length; i++ ){
+      if (a[i] < b[i])
+      return true
+      else if (a[i] > b[i])
+      return false
+      else if (a[i] == b[i])
+      return null
+    }
+    }
+   console.log(awardPoint(a[i] > b[i]))
+
+//3.// @algorithm
+let arrNum = [4, -5, 0];
+function ratioInt (){
+    let posNum = 0;
+    let negNum = 0;
+    let zeroNum = 0;
+    for(let i = 0; i < array.length; i++){
+        if (arrNum[i]>0){
+            posNum++
+        }
+        if (arrNum[i]>0){
+            negNum++
+        }
+        if (arrNum[i]>0){
+            zeroNum++
+        }
+    
+    console.log(parsefloat(posNum/arr.length))   
+    console.log(parsefloat(posNum/arr.length))   
+    console.log(parsefloat(posNum/arr.length))    
+
+}
+ratioInt()
+// 4. create a functfon name and return sum of two numbers.
+function firstFunction (a ,b){
+    let result = a + b;
+    console.log(result);
+
+}
+firstFunction(10 ,10);
+
+
+
